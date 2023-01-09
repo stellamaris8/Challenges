94d7745571471a3938e690b6fd90272108dc0553
Stella Agüero <agu_stella@hotmail.com>
2023-01-09 20:59:53 -0300

    modificaciones

---
+++ challenge 12.js
@@ -0,0 +1,14 @@
+let numberA = prompt ("Escribe un número A");
+let numberB = prompt ("Escribe un número B");
+
+if (numberA===numberB) {
+   alert(" number A es igual a number B"); 
+}
+else {
+    if (numberA>numberB) {
+   alert ("number A es mayor que number B");
+    } 
+else {alert ("number A es menor que number B");
+    
+    }
+ }
