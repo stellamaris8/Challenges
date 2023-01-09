d003701a011cadf4fde41a2e5a6a537c500442ed
Stella Agüero <agu_stella@hotmail.com>
2023-01-09 00:44:03 -0300

    Agregando challenge 11

---
+++ app.js
@@ -0,0 +1,13 @@
+// function calcularAreaTriangulo(base, altura) {
+//     return`El área del triángulo es ${(base * altura) /2}`;
+// }
+
+// console.log (calcularAreaTriangulo(40,30));
+
+
+function calcularAreaCirculo ( PIxr2) {
+    
+    return`El área del círculo es ${(Math.PI * Math.pow(20,2)).toFixed(2)}`;
+}
+
+console.log (calcularAreaCirculo (Math.PI * Math.pow(20,2))) ;
